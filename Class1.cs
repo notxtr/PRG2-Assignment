88f24c6ff02e8a0714803cf109da22acc8dbaac9
notxtr <s10267163@connect.np.edu.sg>
2025-01-21 20:52:03 +0800

    test

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Flight
+{
+    private string flightNumber;
+    private string origin;
+    private string destination;
+    private DateTime expectedTime;
+    private string status;
+}
