a1c189236cb89696183ac3c0c29cc0460f61ccb6
Aidan-Foo <s10266849@connect.np.edu.sg>
2025-01-24 13:46:25 +0800

    Add some class files (Airline, Terminal, BoardingGate).

---
+++ Airline.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2REAL_assignment
+{
+    class Airline
+    {
+        // attributes
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string code;
+
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        // dictionary to store flights
+        private Dictionary<string, Flight>;
+        public Dictionary<string, Flight> flightDict { get; set; }
+                                        = new Dictionary<string, Flight>();
+
+    }
+}
