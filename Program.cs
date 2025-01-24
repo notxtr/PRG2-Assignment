90f1c7f35465803e79bf5b7991ecd46acc15cd81
notxtr <s10267163@connect.np.edu.sg>
2025-01-24 17:13:39 +0800

    added names

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//==========================================================
+// Student Number : S10267163
+// Student Name : Caden Wong
+// Partner Name : Aidan Foo
+//==========================================================
+
 Console.WriteLine("Hello World! Test message.");
