1c823ae7db06a48b2905d9638572a4ed7711cbfe
Aidan-Foo <s10266849@connect.np.edu.sg>
2025-01-25 18:40:24 +0800

    Fully implement BoardingGate class

--- BoardingGate.cs
+++ BoardingGate.cs
@@ -8,5 +8,43 @@ namespace PRG2REAL_assignment
 {
     class BoardingGate
     {
+        // Attributes
+        private string gateName;
+        private bool supportsCFFT;
+        private bool supportsDDJB;
+        private bool supportsLWTT;
+        private Flight flight;
+
+        // Properties 
+        public string GateName { get; set; }
+        public bool SupportsCFFT { get; set; }
+        public bool SupportsDDJB { get; set; }
+        public bool SupportsLWTT { get; set; }
+        public Flight Flight { get; set; }
+
+        // Default constructor 
+        public BoardingGate() { }
+
+        // Parameterized constructor
+        public BoardingGate(string gn, bool sCFFT, bool sDDJB, bool sLWTT, Flight f)
+        {
+            GateName = gn;
+            SupportsCFFT = sCFFT;
+            SupportsDDJB = sDDJB;
+            SupportsLWTT = sLWTT;
+            Flight = f;
+        }
+        // Method to calculate fees
+        public double CalculateFees()
+        {
+            return Flight.CalculateFees();
+        }
+
+        // ToString method 
+        public override string ToString()
+        {
+            return $"{GateName} Supports CFFT: {SupportsCFFT} Supports DDJB: {SupportsDDJB} " +
+                $"Supports LWTT: {SupportsLWTT} Flight: {Flight}";
+        }
     }
 }
