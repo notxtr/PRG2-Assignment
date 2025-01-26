bc4f98d9594d5d7772f15c5e933b06689f3b6189
Aidan-Foo <s10266849@connect.np.edu.sg>
2025-01-26 16:00:55 +0800

    Added comments and fixed Flight class

--- Flight.cs
+++ Flight.cs
@@ -1,52 +1,58 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 //==========================================================
 // Student Number : S10267163
 // Student Name : Caden Wong
 // Partner Name : Aidan Foo
 //==========================================================
-public class Flight
+
+namespace PRG2REAL_assignment
 {
-    private string flightNumber;
-    private string origin;
-    private string destination;
-    private DateTime expectedTime;
-    private string status;
-
-    public string FlightNumber { get { return flightNumber; } set { flightNumber = value; } }
-    public string Origin { get { return origin; } set { origin = value; } }
-    public string Destination { get { return destination; } set { destination = value; } }
-    public DateTime ExpectedTime { get { return expectedTime; } set { expectedTime = value; } }
-
-    public string Status { get { return status; } set { status = value; } }
-
-    public Flight(string _flightNumber, string _origin, string _destination, DateTime _expectedTime, string _status)
+    // Abstract class
+    abstract class Flight
     {
-        FlightNumber = _flightNumber;
-        Origin = _origin;
-        Destination = _destination;
-        ExpectedTime  = _expectedTime;
-        Status = _status;
-    }
+        // Attributes
+        private string flightNumber;
+        private string origin;
+        private string destination;
+        private DateTime expectedTime;
+        private string status;
 
-    public double CalculateFees()
-    {
-        double fee = 300;
-        if (Origin == "Singapore (SIN)") // Departing flight fee
+        // Properties
+        public string FlightNumber { get { return flightNumber; } set { flightNumber = value; } }
+        public string Origin { get { return origin; } set { origin = value; } }
+        public string Destination { get { return destination; } set { destination = value; } }
+        public DateTime ExpectedTime { get { return expectedTime; } set { expectedTime = value; } }
+        public string Status { get { return status; } set { status = value; } }
+
+        // Default Constructor 
+        public Flight() { }
+
+        // Parameterized Constructor
+        public Flight(string _flightNumber, string _origin, string _destination, DateTime _expectedTime, string _status)
         {
-            fee += 800;
-        }
-        else if (Destination == "Singapore (SIN)") // Arriving flight fee
-        {
-            fee += 500;
+            FlightNumber = _flightNumber;
+            Origin = _origin;
+            Destination = _destination;
+            ExpectedTime = _expectedTime;
+            Status = _status;
         }
 
-        return fee;
-    }
-    public override string ToString()
-    {
-        return $"{FlightNumber}\t{Origin}\t\t{Destination}\t\t{ExpectedTime}";
+        // Virtual method to be overridden by subclasses
+        public virtual double CalculateFees()
+        {
+          return 0;
+        }
+
+        // ToString method 
+        public override string ToString()
+        {
+            return $"{FlightNumber}\t{Origin}\t\t{Destination}\t\t{ExpectedTime}";
+        }
     }
 }
-
-
