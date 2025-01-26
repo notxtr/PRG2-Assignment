e20fa99c107daeba1ade1c0a724612bfa4632bf3
Aidan-Foo <s10266849@connect.np.edu.sg>
2025-01-26 16:45:50 +0800

    Added comments and fixed NORMFlight class

--- NORMFlight.cs
+++ NORMFlight.cs
@@ -1,20 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 //==========================================================
 // Student Number : S10267163
 // Student Name : Caden Wong
 // Partner Name : Aidan Foo
 //==========================================================
-public class NORMFlight : Flight
+namespace PRG2REAL_assignment
 {
-    public NORMFlight(string fn, string o, string d, DateTime et, string s) : base(fn, o, d, et, s) { }
-    public double CalculateFees()
+    class NORMFlight : Flight
     {
-        double fee = base.CalculateFees();
-        return fee;
-    }
-    
-    public override string ToString()
-    {
-        return base.ToString();
+        // Default Constructor
+        public NORMFlight() { }
+
+        // Parameterized Constructor
+        public NORMFlight(string fn, string o, string d, DateTime et, string s) : base(fn, o, d, et, s) { }
+
+
+        // Override CalculateFees method
+        public override double CalculateFees()
+        {
+            if (Destination == "SIN")
+            {
+                return 500 + 300;
+            }
+            else
+            {
+                return 800 + 300;
+            }
+        }
+
+        // ToString method
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
