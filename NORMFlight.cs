628a19e98a19dbef122cfa7c5d2390e957e27fd4
notxtr <s10267163@connect.np.edu.sg>
2025-01-26 15:40:27 +0800

    changed

--- NORMFlight.cs
+++ NORMFlight.cs
@@ -7,7 +7,7 @@
 public class NORMFlight : Flight
 {
     public NORMFlight(string fn, string o, string d, DateTime et, string s) : base(fn, o, d, et, s) { }
-    public void CalculateFees() { }
+    public void CalculateFees() { } //Empty method since there is no additional fees for a normal flight
 
     public override string ToString()
     {
