37661edfb184c1c95058522ecfbda1660c6c530b
notxtr <s10267163@connect.np.edu.sg>
2025-01-27 00:16:40 +0800

    added feature 8 (not fully fixed)

--- Program.cs
+++ Program.cs
@@ -107,7 +107,7 @@ void ListAllFlights(List<Airline> airlineList)
                 {
                     notfound2 = false;
                     Console.WriteLine("=============================================\r\nFlight Details\r\n=============================================");
-                    Console.WriteLine();
+                    Console.WriteLine("FlightNumber\tOrigin\t\tDestination\t\tExpectedTime");
                     Console.WriteLine(specificflight.ToString());
                 }
             }
@@ -122,6 +122,132 @@ void ListAllFlights(List<Airline> airlineList)
         Console.WriteLine("Airline code not found.");
     }
 }
+
+// Modify flight details
+void ModifyFlight(List<Flight> flightList)
+{
+    Console.WriteLine("=============================================\r\nList of Airlines for Changi Airport Terminal 5\r\n=============================================");
+    foreach (var airline in AirlineList)
+    {
+        Console.WriteLine(airline.ToString());
+    }
+    Console.WriteLine("\nEnter Airline code: ");
+    string code = Console.ReadLine();
+    bool notfound = true; // Boolean for checking if airline code is found
+    foreach (var airline in AirlineList)
+    {
+        if (airline.Code == code)
+        {
+            notfound = false;
+            Console.WriteLine($"=============================================\r\nList of Flights for {airline.Name}\r\n=============================================");
+            Console.WriteLine("FlightNumber\tOrigin\t\tDestination\t\tExpectedTime");
+            foreach (var flight in airline.Flights.Values)
+            {
+                Console.WriteLine(flight.ToString());
+            }
+
+            Console.WriteLine("Enter a specific flight number to modify or delete: ");
+            string flightnumber = Console.ReadLine();
+            bool notfound2 = true; // Boolean for checking if flight number is found
+            foreach (var specificflight in airline.Flights.Values)
+            {
+
+                if (specificflight.FlightNumber == flightnumber)
+                {
+                    notfound2 = false;
+                    Console.WriteLine("1. Modify Flight");
+                    Console.WriteLine("2. Delete Flight");
+                    Console.WriteLine("Please select your option: ");
+                    try
+                    {
+                        int option = Convert.ToInt32(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 1:
+                                Console.WriteLine("1. Modify Basic Information\r\n2. Modify Status\r\n3. Modify Special Request Code\r\n4. Modify Boarding Gate ");
+                                Console.WriteLine("Please select your option: ");
+                                int option2 = Convert.ToInt32(Console.ReadLine());
+                                switch (option2)
+                                {
+                                    case 1:
+                                        
+                                        Console.WriteLine("Enter new origin: ");
+                                        string neworigin = Console.ReadLine();
+                                        Console.WriteLine("Enter new destination: ");
+                                        string newdestination = Console.ReadLine();
+                                        Console.WriteLine("Enter new expected time: ");
+                                        DateTime newexpectedtime = Convert.ToDateTime(Console.ReadLine());
+                                        specificflight.Origin = neworigin;
+                                        specificflight.Destination = newdestination;
+                                        specificflight.ExpectedTime = newexpectedtime;
+                                        Console.WriteLine("Flight details modified.");
+
+                                        break;
+                                    case 2:
+                                        Console.WriteLine("Enter new status: ");
+                                        string newstatus = Console.ReadLine();
+                                        specificflight.Status = newstatus;
+                                        break;
+                                    case 3:
+                                        Console.WriteLine("Enter new special request code: ");
+                                        string newspecialrequestcode = Console.ReadLine();
+                                        specificflight.Code = newspecialrequestcode;
+                                        break;
+                                    case 4:
+                                        Console.WriteLine("Enter new boarding gate: ");
+                                        string newboardinggate = Console.ReadLine();
+                                        specificflight.Gate = newboardinggate;
+                                        break;
+                                    default:
+                                        Console.WriteLine("Invalid integer. Please try again.");
+                                        break;
+                                }
+                                break;
+                            case 2:
+                                if (airline.Flights.ContainsKey(flightnumber))
+                                {
+                                    Console.WriteLine("Confirm deletion of flight? (Y/N)");
+                                    string confirm = Console.ReadLine().ToUpper();
+                                    if (confirm == "Y")
+                                    {
+                                        airline.Flights.Remove(flightnumber);
+                                        Console.WriteLine("Flight Removed.");
+                                    }
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("Invalid integer. Please try again.");
+                                break;
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Invalid input. Please try again.");
+                       
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Invalid input. Please try again.");
+                       
+                    }
+
+
+                }
+            }
+            if (notfound2)
+            {
+                Console.WriteLine("Flight number not found.");
+            }
+
+
+        }
+    }
+    if (notfound)
+    {
+        Console.WriteLine("Airline code not found.");
+    }
+}
+
 
 CreateAirlineObject(AirlineList);
 CreateBoardingGateObject(BoardingGateList);
@@ -149,6 +275,7 @@ while (trueornot == true)
                 ListAllFlights(AirlineList);
                 break;
             case 6:
+                ModifyFlight(FlightList);
                 break;
             case 7:
                 break;
