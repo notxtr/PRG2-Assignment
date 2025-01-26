5886d5895ddd3c5b84da1a549aad2e2214ca91a8
Aidan-Foo <s10266849@connect.np.edu.sg>
2025-01-26 16:39:38 +0800

    Added comments and fixed DDJBFlight class

--- DDJBFlight.cs
+++ DDJBFlight.cs
@@ -1,27 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 //==========================================================
 // Student Number : S10267163
 // Student Name : Caden Wong
 // Partner Name : Aidan Foo
 //==========================================================
-public class DDJBFlight : Flight
+
+namespace PRG2REAL_assignment
 {
-    private double requestFee;
-    public double RequestFee { get { return requestFee; } set { requestFee = value; } }
-    public DDJBFlight(string fn, string o, string d, DateTime et, string s) : base(fn, o, d, et, s) { }
-
-
-    public double CalculateFees()
+    class DDJBFlight : Flight
     {
-        double fee = base.CalculateFees();
-        RequestFee = 300;
-        fee += RequestFee;
-        return fee;
-    }
+        // Attribute
+        private double RequestFee()
+        {
+            return 300; // Set default request fee to $300 for DDJB flights
+        }
 
-    public override string ToString()
-    {
-        return base.ToString();
+        // Default Constructor
+        public DDJBFlight() { }
+
+        // Parameterized Constructor
+        public DDJBFlight(string fn, string o, string d, DateTime et, string s) : base(fn, o, d, et, s) { }
+
+        // Override CalculateFees method
+        public override double CalculateFees()
+        {
+            if (Destination == "SIN")
+            {
+                return 500 + 300 + RequestFee();
+            }
+            else
+            {
+                return 800 + 300 + RequestFee();
+            }
+        }
+
+        // ToString method 
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
-
